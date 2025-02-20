b8a5dc2170edcc87281c1068e642fe7145fb7170
Gayathri012003 <pusalagayathri01@gmail.com>
2025-02-20 10:56:09 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,25 @@
+#Datatypes
+    #integer
+a=4
+print(type(a),a)
+   #float
+b=3.5
+print(type(b),b)
+   #string
+c="hello"
+print(type(c),c)
+   #boolean
+d=True
+print(type(d),d)
+   #list
+d=[1,2,"hi",3]
+print(type(d),d)
+   #tuple
+d=("hi",4,5,6)
+print(type(d),d)
+  #set
+d={1,2,3}
+print(type(d),d)
+  #dictionary
+d={1:"anu",2:"anji"}
+print(type(d),d)
