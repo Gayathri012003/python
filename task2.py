b7dae3dfa3bdc4b5bc2a9066ee59c7a553af621a
Gayathri012003 <pusalagayathri01@gmail.com>
2025-02-20 23:07:13 +0530

    task2.py

---
+++ task2.py
@@ -0,0 +1,39 @@
+#1
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+c=int(input("enter a number:"))
+if a>b:
+    if a>c:
+        print("a is big")
+    else:
+        print("c is big")
+elif b>c:
+    print("b is big")
+else:
+    print("c is big")
+#2
+year=int(input("enter year:"))
+if (year%4==0 and year%100!=0) or year%400==0:
+            print("leap year")
+else:
+        print("not leap year")
+#4
+Grade=int(input("enter grade of the student:"))
+if Grade>100:
+    print("not defined")
+elif Grade>=90 and Grade<=100:
+    print("Grade A")
+elif Grade>=80 and Grade<=89:
+    print("Grade B")
+elif Grade>=70 and Grade<=79:
+    print("Grade C")
+else:
+    print("Fail")
+#4
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+c=int(input("enter a number:"))
+if a+c>b and a+b>c and b+c>a:
+    print("valid triangle")
+else:
+    print("not valid")
