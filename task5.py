4827ba0cbe0738cb51f52b705b51819afae9f4c6
Gayathri012003 <pusalagayathri01@gmail.com>
2025-03-04 12:30:24 +0530

    Create task5.py

---
+++ task5.py
@@ -0,0 +1,40 @@
+a=5
+if a>0:
+    print("a is positive")
+print("time complexity-O(1)")
+print("space complexity-O(1)")
+#else condition
+a=5
+if a<0:
+    print("a is negative")
+else:
+    print("a is positive")
+print("time complexity-O(1)")
+print("space complexity-O(1)")
+#elif
+a=-10
+if a==0:
+    print("a is zero")
+elif a>0:
+    print("a is positive")
+else:
+    print("a is negative")
+print("looping statements--------------------")
+print("time complexity-O(1)")
+print("space complexity-O(1)")
+#while loop
+a=1
+while a<6:
+    print(a)
+    a=a+1
+print("time complexity-O(1)")
+print("space complexity-O(1)")
+
+#for loop
+a=11
+for i in range(1,a):
+    print(i)
+print("jumping statements--------------------")
+print("time complexity-O(n)")
+print("space complexity-O(1)")
+
