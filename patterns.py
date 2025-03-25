72edd33034b0219d7ca5922667a9fae79931b4f1
Gayathri012003 <pusalagayathri01@gmail.com>
2025-03-25 16:05:23 +0530

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -138,3 +138,46 @@ for i in range(n):
                 print(" ",end=" ")
     print( )'''
 
+'''enter a num5
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+n=int(input("enter a num"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+'''
+'''enter a number:5
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15
+num=int(input("enter a number:"))
+count=1
+for i in range(num):
+    for j in range(num):
+        if i>=j:
+            print(count,end=" ")
+            count+=1
+    print()
+ 
+ enter a number:5
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+    
+num=int(input("enter a number:"))
+for i in range(num):
+    for k in range(1,num-i):
+        print(" ",end="")
+    for j in range(num):
+        if i>=j:
+            print("*",end=" ")
+    print()
+'''
