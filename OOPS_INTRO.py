ee2e6f14e5c8254bc8c00ad31fc5526a5a25356d
Gayathri012003 <pusalagayathri01@gmail.com>
2025-03-25 15:43:16 +0530

    Create OOPS_INTRO.py

---
+++ OOPS_INTRO.py
@@ -0,0 +1,34 @@
+#OOPS CONCEPTS
+'''
+class parent:
+    print("this is parent")
+class child(parent):
+    print("this is child")
+a=child()'''
+'''class add:
+    def adding(self,a,b):
+        print(a+b)
+b=add() 
+b.adding(2,3)
+
+
+#constructor in class
+class human:
+    def __init__(self,name,age):
+        print("this is constructor")
+        self.name=name
+        self.age=age
+        print("Hello this is",self.name,".I am",self.age,"years old")
+aa=human("Gayathri",'21')
+b=human("anu","20")
+
+
+
+class add:
+    def adding(self,a,b):
+        print(a+b)
+    def subtract(self,a,b):
+        print(a-b)
+a=add()
+a.adding(2,3)
+a.subtract(3,2)'''
